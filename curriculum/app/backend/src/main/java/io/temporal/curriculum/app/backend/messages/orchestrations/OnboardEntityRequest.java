3131179bccf59e3ec4881cc3f43d33492956f706
Mike Nichols <mike.nichols@temporal.io>
2024-06-30 18:52:58 +0200

    remove curriculum artifacts

--- curriculum/app/backend/src/main/java/io/temporal/curriculum/app/backend/messages/orchestrations/OnboardEntityRequest.java
+++
@@ -1,8 +0,0 @@
-package io.temporal.curriculum.app.backend.messages.orchestrations;
-
-public record OnboardEntityRequest(
-    String id,
-    String value,
-    int completionTimeoutSeconds,
-    String deputyOwnerEmail,
-    boolean skipApproval) {}
--- curriculum/app/backend/src/main/java/io/temporal/curriculum/app/backend/orchestrations/OrchestrationsConfig.java
+++
@@ -1,8 +0,0 @@
-package io.temporal.curriculum.app.backend.orchestrations;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class OrchestrationsConfig {}
