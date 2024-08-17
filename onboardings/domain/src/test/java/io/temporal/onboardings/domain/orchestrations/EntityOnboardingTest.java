0ebcbda89ecd3df1c5f3b4b5fa925ebbb5b985c5
Mike Nichols <mike.nichols@temporal.io>
2024-08-16 23:03:49 -0400

    trying to pass in mock

--- onboardings/domain/src/test/java/io/temporal/onboardings/domain/orchestrations/EntityOnboardingTest.java
+++ onboardings/domain/src/test/java/io/temporal/onboardings/domain/orchestrations/EntityOnboardingTest.java
@@ -8,12 +8,22 @@ import io.temporal.client.WorkflowOptions;
 import io.temporal.failure.ApplicationFailure;
 import io.temporal.onboardings.domain.DomainConfig;
 import io.temporal.onboardings.domain.integrations.IntegrationsHandlers;
+import io.temporal.onboardings.domain.messages.commands.ApproveEntityRequest;
+import io.temporal.onboardings.domain.messages.commands.RegisterCrmEntityRequest;
+import io.temporal.onboardings.domain.messages.commands.RequestDeputyOwnerApprovalRequest;
+import io.temporal.onboardings.domain.messages.orchestrations.Errors;
 import io.temporal.onboardings.domain.messages.orchestrations.OnboardEntityRequest;
+import io.temporal.onboardings.domain.messages.queries.EntityOnboardingState;
+import io.temporal.onboardings.domain.messages.values.ApprovalStatus;
+import io.temporal.onboardings.domain.notifications.NotificationsHandlers;
 import io.temporal.testing.TestWorkflowEnvironment;
+import java.time.Duration;
 import java.util.Objects;
 import java.util.UUID;
 import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -41,12 +51,14 @@ public class EntityOnboardingTest {
   @Autowired WorkflowClient workflowClient;
 
   @MockBean CrmListener listener;
-  //  @MockBean IntegrationsHandlers integrationsHandlers;
-  //
-  //  @Bean
-  //  public IntegrationsHandlers getIntegrationsHandlersBean() {
-  //    return integrationsHandlers;
-  //  }
+
+  @Autowired
+  @Qualifier("integrations-handlers")
+  IntegrationsHandlers integrationsHandlers;
+
+  @Autowired
+  @Qualifier("notifications-handlers")
+  NotificationsHandlers notificationsHandlers;
 
   @Value("${spring.temporal.workers[0].task-queue}")
   String taskQueue;
@@ -62,14 +74,20 @@ public class EntityOnboardingTest {
   }
 
   @Test
-  public void givenValidArgs_itShouldExecuteWorkflow() {
+  public void givenValidArgsWithOwnerApprovalNoDeputyOwner_itShouldOnboardTheEntity() {
     String wfId = UUID.randomUUID().toString();
-    var args = new OnboardEntityRequest(wfId, UUID.randomUUID().toString(), 3, null, true);
+    var args = new OnboardEntityRequest(wfId, UUID.randomUUID().toString(), 3, null, false);
     EntityOnboarding sut =
         workflowClient.newWorkflowStub(
             EntityOnboarding.class,
             WorkflowOptions.newBuilder().setWorkflowId(wfId).setTaskQueue(taskQueue).build());
     WorkflowClient.execute(() -> sut.execute(args));
+    testWorkflowEnvironment.sleep(Duration.ofSeconds(1));
+    sut.approve(new ApproveEntityRequest("nocomment"));
+
+    verify(integrationsHandlers, times(1)).registerCrmEntity(argThat(arg -> arg.id().equals(wfId)));
+    EntityOnboardingState response = sut.getState();
+    Assertions.assertEquals(response.approval().approvalStatus(), ApprovalStatus.APPROVED);
   }
 
   @Test
@@ -88,7 +106,8 @@ public class EntityOnboardingTest {
               sut.execute(args);
             });
     Assertions.assertInstanceOf(ApplicationFailure.class, e.getCause());
-    Assertions.assertEquals("invalid_args", ((ApplicationFailure) e.getCause()).getType());
+    Assertions.assertEquals(
+        Errors.INVALID_ARGS.name(), ((ApplicationFailure) e.getCause()).getType());
   }
 
   @Test
@@ -113,11 +132,26 @@ public class EntityOnboardingTest {
 
   @ComponentScan
   public static class Configuration {
-    @MockBean private IntegrationsHandlers integrationHandlers;
-
-    @Bean
-    public IntegrationsHandlers getIntegrationsHandlersBean() {
-      return integrationHandlers;
-    }
+//    @MockBean(name = "integrations-handlers")
+//    private IntegrationsHandlers integrationHandlers;
+//
+//    @MockBean(name = "notifications-handlers")
+//    private NotificationsHandlers notificationsHandlers;
+//
+//    @Bean
+//    public IntegrationsHandlers getIntegrationsTestHandlersBean() {
+//      Mockito.doNothing()
+//          .when(integrationHandlers)
+//          .registerCrmEntity(any(RegisterCrmEntityRequest.class));
+//      return integrationHandlers;
+//    }
+//
+//    @Bean
+//    public NotificationsHandlers getNotificationsTestHandlers() {
+//      Mockito.doNothing()
+//          .when(notificationsHandlers)
+//          .requestDeputyOwnerApproval(any(RequestDeputyOwnerApprovalRequest.class));
+//      return notificationsHandlers;
+//    }
   }
 }
